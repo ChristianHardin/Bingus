334f7efa41ebe15e75a499a4b7d17861ba635357
Christian Hardin <christianhardin12345@gmail.com>
2024-05-27 21:57:46 -0400

    feat: centered pad to middle to term

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import curses
 from curses import wrapper
 
 def main(stdscreen):
+    height, width = stdscreen.getmaxyx()
     stdscreen.clear()
-    stdscreen.addstr('test')
     stdscreen.refresh()
-    stdscreen.getkey()
 
+    pad = curses.newpad(5, width-10)
+    for y in range(0, 5):
+        for x in range(0, width-11):
+            pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+
+    pad.refresh(0,0, int((height/2)-3),5, int((height/2)+2),width-10)
+    stdscreen.getkey() 
 
 wrapper(main)
